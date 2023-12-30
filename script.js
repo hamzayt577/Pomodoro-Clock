f47690004a27f54b570708a149e7bc79499712b9
hamzayt577 <hamzayt577@gmail.com>
2023-12-30 15:20:47 +0400

    created markup, gave styles and selected all elements in the javascript

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+const timer = document.getElementById("timer");
+const btn_start = document.getElementById("start");
+const btn_pause = document.getElementById("pause");
+const btn_reset = document.getElementById("reset");
