0997267e2f2cd162954121c191da23b1791e52f2
hamzayt577 <hamzayt577@gmail.com>
2023-12-31 10:49:32 +0400

    made the clock counting from backwards

--- script.js
+++ script.js
@@ -7,33 +7,38 @@ const btn_reset = document.getElementById("reset");
 
 // Initialization
 const init = function () {
-  timer_element.textContent = "00:00:00";
+  timer_element.textContent = "00:25:00";
 };
 
 init();
 
 // timer value variables
-let [seconds, minutes, hours] = [0, 0, 0];
+let [seconds, minutes, hours] = [0, 25, 0];
 let timer_time = null;
 
-// stop watch increment function
+// stop watch decrement function
 const stopwatch = function () {
-  seconds++;
-  if (seconds == 60) {
-    seconds = 0;
-    minutes++;
+  if (seconds > 0 || minutes > 0 || hours > 0) {
+    seconds--;
 
-    if (minutes == 60) {
-      minutes = 0;
-      hours++;
+    if (seconds < 0) {
+      seconds = 59;
+      minutes--;
+
+      if (minutes < 0) {
+        minutes = 59;
+        hours--;
+      }
     }
+
+    let h = hours < 10 ? `0${hours}` : `${hours}`;
+    let m = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    let s = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    timer_element.innerHTML = `${h}:${m}:${s}`;
+  } else {
+    clearInterval(timer_time);
   }
-
-  let h = hours < 10 ? `0${hours}` : `${hours}`;
-  let m = minutes < 10 ? `0${minutes}` : `${minutes}`;
-  let s = seconds < 10 ? `0${seconds}` : `${seconds}`;
-
-  timer_element.innerHTML = `${h}:${m}:${s}`;
 };
 
 // function that starts the timer
@@ -50,8 +55,8 @@ const watch_pause = function () {
 
 const watch_reset = function () {
   clearInterval(timer_time);
-  [seconds, minutes, hours] = [0, 0, 0];
-  timer_element.innerHTML = "00:00:00";
+  [seconds, minutes, hours] = [0, 25, 0];
+  timer_element.innerHTML = "00:25:00";
 };
 
 btn_start.addEventListener("click", watch_start);
